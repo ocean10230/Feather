a9ccb45d1b71c1435165c36c5424d4d4a7bc95dc
Ocean <your-email@example.com>
2026-09-24 10:28:10 +0800

    delete everything

--- src/internal/util.ts
+++
@@ -1,2 +0,0 @@
-export const randomHex = (len: number = 32) => [...Array(len)].map(() => math.floor(math.random() * 16).toString(16)).join('')
-export const sleep = (m: number) => new Promise(r => setTimeout(r, m))
